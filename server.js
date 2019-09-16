9bffa5f6adac79c699599ed06602916b13dad2dd
acorsine <acorsine@santander.com.br>
2019-09-16 15:19:22 -0300

    subindo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const app = express();
+app = express();
  
 app.use(cors);
 app.options('*', cors);
